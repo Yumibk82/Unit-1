e64efea0748e4f80aec70edcc424a158ac469b79
Tran Thi Thu Hang <ttthang0102gmail.com>
2020-10-05 16:05:06 +0700

    update

---
+++ server_time.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Server time</title>
+</head>
+<body>
+<h1 style="color: blue"> Bây giờ là:</h1>
+
+<?php
+
+ echo date('Y-M-d h:m:s');
+?>
+</body>
+</html>
